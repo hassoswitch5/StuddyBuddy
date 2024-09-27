5b9bae05f414305fd89707519d8d64e84a703bae
ZaidEl-turkey <zaidelturkey798@gmail.com>
2024-09-27 11:20:20 +0300

    changes

--- src/TopicsPage.js
+++ src/TopicsPage.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Topic from './topic'; // Import the Topic component
+import '../src/topicspage.css';
+import Topic from './topic';
 
 const TopicsPage = () => {
     const [topics, setTopics] = useState([]);
@@ -54,47 +55,10 @@ const TopicsPage = () => {
         navigate(`/community/${topic}`);
     };
 
-    const gradientStyle = {
-        background: 'linear-gradient(to right, #a2d3e9, #ffffff)',
-        height: '100vh',
-        margin: 0,
-        display: 'flex',
-        flexDirection: 'column',
-    };
-
-    const mainContentStyle = {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        padding: '0 20px',
-    };
-
-    const buttonStyle = {
-        marginTop: '20px',
-        fontSize: '20px',
-        backgroundColor: '#2c94cc',
-        color: 'white',
-        border: 'none',
-        borderRadius: '12px',
-        padding: '10px 20px',
-        cursor: 'pointer',
-    };
-
-    const topicContainerStyle = {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        marginTop: '20px',
-        width: '100%',
-    };
-
     return (
-        <div className="TopicsPage" style={gradientStyle}>
-            <div style={mainContentStyle}>
-                <h1 style={{ marginBottom: '20px', fontWeight: 'bold', fontSize: '60px' }}>
+        <div className="TopicsPage">
+            <div>
+                <h1>
                     Topics
                 </h1>
                 <input
@@ -102,13 +66,13 @@ const TopicsPage = () => {
                     value={newTopic}
                     onChange={(e) => setNewTopic(e.target.value)}
                     placeholder="Enter new topic"
-                    style={{ padding: '10px', fontSize: '16px', marginBottom: '20px', borderRadius: '8px', border: '1px solid #ccc' }}
+                    className='input'
                 />
-                <button onClick={handleAddTopic} style={buttonStyle}>
+                <button onClick={handleAddTopic} className='buttonStyle'>
                     Add Topic
                 </button>
-                {message && <div style={{ marginTop: '10px', fontSize: '16px', color: '#2c94cc' }}>{message}</div>}
-                <div style={topicContainerStyle}>
+                {message && <div>{message}</div>}
+                <div className='topicContainerStyle'>
                     {topics.map((topicObj, index) => (
                         <Topic
                             key={index}
